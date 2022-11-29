93bbda4888681c4d605236548c8d025aa96a69b1
Roman Liukevich <r.liukevich@gmail.com>
2022-11-29 19:13:15 +0100

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Roman Liukevich on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWorm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookWorm
+//
+//  Created by Roman Liukevich on 11/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
